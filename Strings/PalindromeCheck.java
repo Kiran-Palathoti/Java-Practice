a73b9b3f50b95b77a47304ab70c2bb8fd920a98a
Kiran-Palathoti <palathotikiran@gmail.com>
2025-09-18 22:20:56 +0530

    Create PalindromeCheck.java

---
+++ Strings/PalindromeCheck.java
@@ -0,0 +1,34 @@
+class PalindromeCheck {
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+  public static void main(String[] args){
+    String s = "MARKRAM";
+    if(isPalindrome(s){
+      System.out.println("Palindrome");
+}
+    else{
+      System.out.println("Not a Palindrome");
