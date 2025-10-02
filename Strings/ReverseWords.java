65f2b9d61712e6cb237cf411f43a0fa0d28e9955
Kiran-Palathoti <palathotikiran@gmail.com>
2025-10-02 10:21:39 +0530

    Add files via upload

---
+++ Strings/ReverseWords.java
@@ -0,0 +1,15 @@
+public class ReverseWords {
+    public static String reveWords(String s){
+        String[] words = s.split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]+" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(reveWords(s));
+    }
+}
